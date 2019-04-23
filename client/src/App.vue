6fff2e121f50a9e045815cc8aaf74ffe08b624d5
DaXiong1 <zhang_sx09@163.com>
2019-04-23 14:35:45 +0800

    主页前端提交,支持scss

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -11,13 +10,13 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  /*这里必须写成@import形式，标签里面用src引入会报错*/
+  @import '../static/css/style.scss';
+  /*
+    之前图片一直显示不出来就是没加这个样式，必须设置html,body,#app的height都为100%
+   */
+  #app {
+    height:100%;
+  }
 </style>
---
+++ client/src/components/main.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="layout">
+    <header class="header navDown">
+      <section class="box header-content">
+        <nav>
+          <a href="/" class="nuxt-link-active">首页</a>
+          <a href="/article" class="nuxt-link-exact-active nuxt-link-active active">文章</a>
+          <a href="/project" class="">项目</a>
+          <a href="/about" class="">关于</a>
+          <a href="/music" class="">音乐</a>
+        </nav>
+        <div class="searchBox">
+          <div class="search" style="padding-left: 5px;">
+            <div class="text showText">
+              <input type="text" placeholder="你心所想...">
+            </div>
+            <div class="searchIcon">
+              <i class="iconfont rightIcon"></i>
+            </div>
+          </div>
+        </div>
+      </section>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
