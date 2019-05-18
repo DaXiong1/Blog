c020eb0bbf2d2ffa46c62970bd1b4dd4e8007f9b
DaXiong1 <zhang_sx09@163.com>
2019-05-18 15:44:34 +0800

    页面样式

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,6 +12,7 @@ export default {
 
 <style lang="scss">
   /*这里必须写成@import形式，标签里面用src引入会报错*/
+  @import '../static/css/init.css';
   @import '../static/css/style.scss';
   /*
     之前图片一直显示不出来就是没加这个样式，必须设置html,body,#app的height都为100%
--- client/src/components/main.vue
+++ client/src/components/main.vue
@@ -5,9 +5,9 @@
         <nav>
           <a href="/">首页</a>
           <a href="/article" class="active">文章</a>
-          <a href="/project" class="">项目</a>
-          <a href="/about" class="">关于</a>
-          <a href="/music" class="">音乐</a>
+          <a href="/project" class>项目</a>
+          <a href="/about" class>关于</a>
+          <a href="/music" class>音乐</a>
         </nav>
         <div class="searchBox">
           <div class="search" :style="showSearch ? {paddingLeft: '5px'} : {paddingLeft: '0'}">
@@ -15,12 +15,111 @@
               <input type="text" placeholder="你心所想...">
             </div>
             <div class="searchIcon">
-              <i class="iconfont" @click="toShowSearch" v-if="!showSearch">&#xe603;</i><i class="iconfont rightIcon" @click="tohiddenSearch" v-else>&#xe640;</i>
+              <i class="iconfont" @click="toShowSearch" v-if="!showSearch">&#xe603;</i>
+              <i class="iconfont rightIcon" @click="tohiddenSearch" v-else>&#xe640;</i>
             </div>
           </div>
         </div>
       </section>
     </header>
+    <div id="mailContent" class="mailContent">
+      <div class="articleBox" style="display: none;">
+        <main class="box main">
+          <section class="article">
+            <div class="item">
+              <a class="article-title">
+                <h2>哈哈哈，这是标题</h2>
+              </a>
+              <p class="article-desc">啦啦啦啦啦了，这是摘要</p>
+              <div class="article-info">
+                <span class="time">2019.05.18</span>
+                <i class="iconfont">&#xe600;</i>
+                <span class="time">
+                  <strong>1</strong>次阅读
+                </span>
+                <i class="iconfont">&#xe600;</i>
+                <span class="time">
+                  <strong>1</strong>条评论
+                </span>
+                <i class="iconfont">&#xe600;</i>
+                <span class="time">
+                  <strong>1</strong>人喜欢
+                </span>
+              </div>
+            </div>
+            <div class="item">
+              <a class="article-title">
+                <h2>哈哈哈，这是标题</h2>
+              </a>
+              <p class="article-desc">啦啦啦啦啦了，这是摘要</p>
+              <div class="article-info">
+                <span class="time">2019.05.18</span>
+                <i class="iconfont">&#xe600;</i>
+                <span class="time">
+                  <strong>1</strong>次阅读
+                </span>
+                <i class="iconfont">&#xe600;</i>
+                <span class="time">
+                  <strong>1</strong>条评论
+                </span>
+                <i class="iconfont">&#xe600;</i>
+                <span class="time">
+                  <strong>1</strong>人喜欢
+                </span>
+              </div>
+            </div>
+          </section>
+          <section class="sideLabel">
+            <div class="fixedRight">
+              <div class="labels">
+                <div class="title">标签</div>
+                <div class="label-list">
+                  <a>哈哈哈</a>
+                  <a>哈哈哈</a>
+                  <a>哈哈哈</a>
+                  <a>哈哈哈</a>
+                  <a>哈哈哈</a>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+        <div class="sideIcon">
+          <i class="iconfont">&#xe654;</i>
+          <i class="iconfont">&#xe638;</i>
+          <i class="iconfont">&#xe635;</i>
+        </div>
+      </div>
+      <section>
+        <div class="markdown articleDetailBox">
+          <div class="title">
+            <h1>这个是标题哦</h1>
+          </div>
+          <div>cnncibfcbficb</div>
+        </div>
+        <div class="info articleDetailBox">
+          <p>本文于&nbsp;2019/05/18 下午&nbsp;发布，当前已被围观&nbsp;1100&nbsp;次</p>
+          <p>
+            标签：
+            <a href="/article" class="tagLink nuxt-link-active">dcdc</a>
+          </p>
+          <p>作者：大熊弋</p>
+          <p>链接：https://xxx</p>
+          <p>著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。</p>
+        </div>
+        <div class="arcMata articleDetailBox" style="background: transparent">
+            <div class="arcMataInfo">
+                <div class="like">
+                    <i class="iconfont" v-if="true"  style="color: #51ce23">&#xe63b;</i>
+                    <i class="iconfont"  v-else>&#xe65c;</i>
+                    <span>100</span>人喜欢
+                </div>
+                <div class="view"><span>100</span>条评论</div>
+            </div>
+        </div>
+      </section>
+    </div>
+    <footer class="layoutFooter footerFixed">© 2019 - 大熊弋的博客</footer>
   </div>
 </template>
 
