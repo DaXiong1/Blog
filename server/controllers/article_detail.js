5c8105009485fbac4cdc2ffec56e6af9e4d32f09
DaXiong1 <zhang_sx09@163.com>
2019-06-06 18:46:03 +0800

    server api开发

--- server/controllers/article_detail.js
+++ server/controllers/article_detail.js
@@ -14,3 +14,15 @@ export const getByArticleId = async (article_id) => {
   const res = await ArticleDetail.find({'article_id': article_id})
   return res
 }
+
+// 根据article_id修改文章内容
+export const updateByArticleId = async (article_id, opts) => {
+  const res = await ArticleDetail.findOneAndUpdate({'article_id': article_id}, {$set: {content: opts.content}})
+  return res
+}
+
+// 根据article_id删除文章内容
+export const delByArticleId = async (article_id, opts) => {
+  const res = await ArticleDetail.findOneAndRemove({'article_id': article_id})
+  return res
+}
--- server/routers/article_detail.js
+++ server/routers/article_detail.js
@@ -1,5 +1,5 @@
 import {resError, resSuccess} from '../utils/responseHandler'
-import { putContent, getByArticleId } from '../controllers/article_detail'
+import { putContent, getByArticleId, updateByArticleId, delByArticleId } from '../controllers/article_detail'
 import { controller, put, del, post, get, required } from '../decorator'
 import config from '../config'
 
@@ -27,6 +27,32 @@ export class articleDetailController {
       resSuccess({ctx, msg: '添加文章内容成功', result})
     } catch (err) {
       resError({ctx, msg: '查询文章内容失败', err})
+    }
+  }
+
+  // 根据article_id修改文章内容
+  @post('update/:article_id')
+  @required({body: ['content']})
+  async updateContentByArticleId (ctx) {
+    const { article_id } = ctx.params
+    const opts = ctx.request.body
+    try {
+      await updateByArticleId(article_id, opts)
+      resSuccess({ctx, msg: '修改文章内容成功'})
+    } catch (err) {
+      resError({ctx, msg: '修改文章内容失败', err})
+    }
+  }
+
+  // 根据article_id删除文章内容
+  @del('del/:article_id') // 动态路由
+  async delContentByArticleId (ctx) {
+    const { article_id } = ctx.params
+    try {
+      await delByArticleId(article_id)
+      resSuccess({ctx, msg: '删除文章内容成功'})
+    } catch (err) {
+      resError({ctx, msg: '删除文章内容失败', err})
     }
   }
 }
