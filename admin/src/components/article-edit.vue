c8ac0cc1076df242af6740179661dbec7ab5b999
DaXiong1 <zhang_sx09@163.com>
2019-06-25 10:56:42 +0800

    markdown编辑器样式修改

--- admin/src/components/article-edit.vue
+++ admin/src/components/article-edit.vue
@@ -2,7 +2,7 @@
     <div class="article_edit">
         <el-input v-model="input_title" placeholder="请输入标题..." maxlength="50" show-word-limit></el-input>
         <el-input v-model="input_desc" placeholder="请输入描述..." maxlength="100" type="textarea" show-word-limit></el-input>
-        <mavon-editor v-model="mdcontent" ref=md :ishljs="true" :boxShadow="false" style="min-height: 600px;height: 600px;" @change="changeMavon" />
+        <mavon-editor v-model="mdcontent" ref=md :ishljs="true" :boxShadow="false" :toolbars="toolbars" style="min-height: 600px;height: 600px;" @change="changeMavon" />
         <el-button type="primary" style="margin-top: 20px;position: absolute;right: 35px;" @click="save">保 存</el-button>
     </div>
 </template>
@@ -17,7 +17,42 @@ export default {
       input_desc: '',
       mdcontent: '',
       type: 1, // 1:编辑文章，2：新增文章
-      article_id: '' // type为1时才有值
+      article_id: '', // type为1时才有值
+      toolbars: {
+          bold: true, // 粗体
+          italic: true, // 斜体
+          header: true, // 标题
+          underline: true, // 下划线
+          strikethrough: true, // 中划线
+          mark: true, // 标记
+          superscript: true, // 上角标
+          subscript: true, // 下角标
+          quote: true, // 引用
+          ol: true, // 有序列表
+          ul: true, // 无序列表
+          link: true, // 链接
+          imagelink: true, // 图片链接
+          code: true, // code
+          table: true, // 表格
+          fullscreen: true, // 全屏编辑
+          readmodel: true, // 沉浸式阅读
+          htmlcode: true, // 展示html源码
+          help: true, // 帮助
+          /* 1.3.5 */
+          undo: true, // 上一步
+          redo: true, // 下一步
+          trash: true, // 清空
+          save: true, // 保存（触发events中的save事件）
+          /* 1.4.2 */
+          navigation: true, // 导航目录
+          /* 2.1.8 */
+          alignleft: true, // 左对齐
+          aligncenter: true, // 居中
+          alignright: true, // 右对齐
+          /* 2.2.1 */
+          subfield: true, // 单双栏模式
+          preview: true // 预览
+      }
     }
   },
   watch: {
